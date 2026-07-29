c2bec46d8cde195d7032bb0a51e207386d83d966
Maryam Shahid <maryam.shahid@ibm.com>
2026-07-28 21:34:03 -0500

    updated ami

--- main.tf
+++ main.tf
@@ -19,8 +19,12 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_ssm_parameter" "amazon_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = data.aws_ssm_parameter.amazon_linux.value
   instance_type = "t2.micro"
 
   tags = {
