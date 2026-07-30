a4d8a6c25a74f83e1039d2e4400bac348af75871
Maryam Shahid <maryam.shahid@ibm.com>
2026-07-29 21:36:04 -0500

    removed variable

--- variables.tf
+++ variables.tf
@@ -6,9 +6,3 @@ variable "suffix" {
   type    = string
   default = "mshahid-072826-a13"
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
